3a1c58c2b66f8815dd193f992486257ab783872b
j54kim <j54kim@uwaterloo.ca>
2024-06-10 02:16:41 -0400

    .

--- setup/create_table/create_table.sql
+++ setup/create_table/create_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE ServiceUser (
     user_id INT GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    password BINARY(64) NOT NULL,
+    password VARCHAR(64) NOT NULL,
     created_at DATE NOT NULL,
         PRIMARY KEY(user_id)
 );
@@ -14,12 +14,13 @@ CREATE TABLE Problem (
     created_by INT,
     created_at DATE NOT NULL,
         PRIMARY KEY(problem_id),
-        FOREIGN KEY(created_by) REFERENCES ServiceUser(user_id)
+        FOREIGN KEY(created_by) REFERENCES ServiceUser(user_id) ON DELETE SET NULL
 );
 
 CREATE TABLE TestCase (
-    problem_id INT ,
+    testcase_id INT GENERATED ALWAYS AS IDENTITY,
+    problem_id INT,
     input TEXT,
     output TEXT,
-    FOREIGN KEY(problem_id) REFERENCES Problem(problem_id)
+    FOREIGN KEY(problem_id) REFERENCES Problem(problem_id) ON DELETE CASCADE
 );

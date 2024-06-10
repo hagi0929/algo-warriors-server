e0e86d2b5be950885c0550395cc2ce03b7e96a3c
j54kim <j54kim@uwaterloo.ca>
2024-06-10 01:12:52 -0400

    hotfix

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 # CS348 Project Server
 
 ### Prerequisite
-miniconda
+miniconda - linux
 ```
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+```
+miniconda - mac
+```
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 rm -rf ~/miniconda3/miniconda.sh
 ~/miniconda3/bin/conda init bash
---
+++ setup/create_table/create_table.sql
@@ -0,0 +1,25 @@
+CREATE TABLE ServiceUser (
+    user_id INT GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password BINARY(64) NOT NULL,
+    created_at DATE NOT NULL,
+        PRIMARY KEY(user_id)
+);
+
+CREATE TABLE Problem (
+    problem_id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(50),
+    description TEXT,
+    created_by INT,
+    created_at DATE NOT NULL,
+        PRIMARY KEY(problem_id),
+        FOREIGN KEY(created_by) REFERENCES ServiceUser(user_id)
+);
+
+CREATE TABLE TestCase (
+    problem_id INT ,
+    input TEXT,
+    output TEXT,
+    FOREIGN KEY(problem_id) REFERENCES Problem(problem_id)
+);

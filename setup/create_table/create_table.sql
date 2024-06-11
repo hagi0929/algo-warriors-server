a852817a68e3f8b02280dd5e09fca10217fba9bb
Kevin Ko <kevin97015@gmail.com>
2024-06-10 22:40:57 -0400

    updating tables needed for user features

--- setup/create_table/create_table.sql
+++ setup/create_table/create_table.sql
@@ -7,6 +7,8 @@ CREATE TABLE ServiceUser (
         PRIMARY KEY(user_id)
 );
 
+
+
 CREATE TABLE Problem (
     problem_id INT GENERATED ALWAYS AS IDENTITY,
     title VARCHAR(50),
@@ -24,3 +26,37 @@ CREATE TABLE TestCase (
     output TEXT,
     FOREIGN KEY(problem_id) REFERENCES Problem(problem_id) ON DELETE CASCADE
 );
+
+CREATE TABLE Role (
+    role_id INT GENERATED ALWAYS AS IDENTITY,
+    role_name TEXT,
+
+    PRIMARY KEY(role_id)
+);
+
+CREATE TABLE UserRole (
+    user_id INT,
+    role_id INT,
+
+    FOREIGN KEY(user_id) REFERENCES ServiceUser(user_id) ON DELETE CASCADE,
+    FOREIGN KEY(role_id) REFERENCES Role(role_id) ON DELETE SET NULL,
+    PRIMARY KEY(user_id, role_id)
+);
+
+CREATE TABLE ContestParticipant (
+    contest_id INT,
+    score INT,
+    user_id INT,
+    
+    FOREIGN KEY(contest_id) REFERENCES Contest(contest_id) ON DELETE CASCADE,
+    FOREIGN KEY(user_id) REFERENCES ServiceUser(user_id) ON DELETE CASCADE,
+    PRIMARY KEY(contest_id, user_id)
+);
+
+CREATE TABLE Permission (
+    permission_id INT GENERATED ALWAYS AS IDENTITY,
+    permission_name TEXT,
+
+    PRIMARY_KEY(permission_id)
+);
+
